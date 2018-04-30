e1e552f8d1a46cd97df890e409f9e8578a25f4d3
Andy Chu <andy.chuatwork@gmail.com>
2018-04-29 23:21:07 -0400

    Combined Weather.js and Index.js

--- index.js
+++ index.js
@@ -154,3 +154,39 @@ var waypoint3 = new Waypoint({
   },
   offset: '70%'
 })
+
+var getWeather = new XMLHttpRequest();
+var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?id=5992500&APPID=c6dd605771239a0a4ca7bccf86ce08ec&units=metric';
+
+getWeather.open("GET", weatherURL, true);
+getWeather.send();
+
+getWeather.onreadystatechange = function getIcon()
+{
+  if (this.readyState == 4 && this.status == 200)
+  {
+    var weatherArray = JSON.parse(this.responseText);
+    var year = (new Date()).getFullYear();
+    
+    determineIcon(weatherArray.weather["0"].icon);
+
+    var temp = Number(Math.round(weatherArray.main.temp+'e1')+'e-1');
+
+    document.getElementById('Temperature').innerHTML = temp;
+    document.getElementById('year').innerHTML = year;
+  }
+}
+
+function determineIcon(codeID)
+{
+  var iconID = codeID;
+  var windowHeight = window.innerHeight;
+  var navBarHeight = $('.navbar').height();
+  var getDownButtonHeight = $('#section05').height();
+  var getHelloWordHeight = $('.intro_words').height();
+  var getIconWidth = $('#icon').width();
+
+  $('#icon').children('img').attr('src', '<source here>');
+
+  document.getElementById('icon').innerHTML = "<img class = \"weatherIcon\" src=\"./weather_icons/"+iconID+".png\"/>";
+}
